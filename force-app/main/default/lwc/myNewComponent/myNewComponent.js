494d6b2ec270c58227b4e981fc10458ec3218899
KateLis84 <emma12022002@gmail.com>
2022-12-14 16:35:53 +0200

    new changes

--- force-app/main/default/lwc/myNewComponent/myNewComponent.js
+++ force-app/main/default/lwc/myNewComponent/myNewComponent.js
@@ -1,19 +1,14 @@
-import { LightningElement } from 'lwc';
-import STUDENT_OBJECT from '@salesforce/schema/Student__c';
-import STUDENT_NAME from '@salesforce/schema/Student__c.Name__c';
-import STUDENT_SCHOLARSHIP from '@salesforce/schema/Student__c.Scholarship__c';
+import { LightningElement, wire, api} from 'lwc';
+import allEnrollments from '@salesforce/apex/GetAllClassEnrollment.gerEnrollments'
+
+const columns = [
+    { label: 'Subject', fieldName: 'name' },
+    { label: 'Grade', fieldName: 'grade'}
+];
+
 
 export default class MyNewComponent extends LightningElement {
-    objectApiName=STUDENT_OBJECT;
-    fields = [STUDENT_NAME, STUDENT_SCHOLARSHIP];
-
-    handleSuccess(event){
-        const toastEvent=new ShowToastEvent({
-            title: 'Student Created!', 
-            message: "Student created: " + event.details.id,
-            variant: 'success'
-        });
-        this.dispatchEvent(toastEvent);
-        
-    }
+    @api recordId;
+    columns = columns;
+    @wire(allEnrollments, {recordId: '$recordId'}) dataEnrollments;
 }
